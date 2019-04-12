86ff5a3e3d05b169a43d961ec40d9a322d05efce
MrNoForks <testingiosapp12345@gmail.com>
2019-04-12 14:59:00 +0530

    Turning off opaque and making background color clear of view

--- PlayingCard/PlayCardView.swift
+++ PlayingCard/PlayCardView.swift
@@ -20,7 +20,8 @@ class PlayCardView: UIView {
         UIColor.white.setFill()
         roundedRect.fill()
         
-
+        //But we wont get to see white rounded corner as background color of view is white and we have to make it clear to make it see through and turn opaque off as it is by default is turned on for performance as it is assumed that you dont have any see through parts, no transparency  and it can be more efficient when it draws.So if we do use transparency which is less efficient but we need it here as we want our corners to show through. we have to turn opaque off if you are going to do anything transparent in your view
+        //Done in storyboard
     }
     
     
