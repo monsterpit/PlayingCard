1c9b605801c4d49d6773be62f671c177ab47cac9
MrNoForks <testingiosapp12345@gmail.com>
2019-04-12 13:04:51 +0530

    Drawing using context of CoreGraphics but it consumes path once a color is filled and other can't be applied . So its a advantage on UIBezierPath has it doesn't consumes its path and you can  to build path over and over again

--- PlayingCard/PlayCardView.swift
+++ PlayingCard/PlayCardView.swift
@@ -10,14 +10,41 @@ import UIKit
 
 class PlayCardView: UIView {
     
-    //stub override func draw(_ rect: CGRect) is commented out because iOS looks to see you have a draw rect and if you do it makes an offScreen buffer for you and kind of preparations for you to draw and that's not cheap so if you dont actually draw in draw rect then you want it to comment it out
-    // Now why would you have a UIView  or UIview subclass that doesn't have a draw rect. Well it's quite common you do all your drawing with subviews , Consider UI StackView , it does all drawing with views that are stacked inside of it.It doesn't do any actual drawing itself , it has no draw rect
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+//drawing some cards with subviews and drawing some with drawRect
+    
+    //and whenever system needs to draw our custom view it will use draw(_ rect: CGRect)
     override func draw(_ rect: CGRect) {
         // Drawing code
+        
+        //drawing circle using coregraphics , in core graphics we always get the context first
+        //we cant draw in core graphics without a context and we get that using UIGraphicsGetCurrentContext()
+        //UIGraphicsGetCurrentContext could return nil but it will never return nil insidde your drawRect it might return nil in other contexts but in draw(_ rect: CGRect) it will always return
+        if let context = UIGraphicsGetCurrentContext(){
+            
+            // bounds gives rectangles that specifies the drawing area
+            //start angle and end angle are in radians not 0 to 360, its radians 0 to 2 pi
+            // 0 is off to the right 0 is not straight up
+            //2 * CGFloat.pi  back to same place i.e. like 360 degrees
+            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+            
+            // line width
+            context.setLineWidth(5)
+            
+            UIColor.green.setFill()
+            
+            #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1).setStroke()
+            
+            //It wont stroke and fill the reason for that is when we draw in an context , it's actually slightly different than using that UIBezierPath
+            //In context when we do strokePath like this it consumes the path. It uses up the path
+            //so when we do the fillPath on the next line , there's no path. We have to start again
+            // so thats the one of the biggest advantage of UIBezierPath
+            context.strokePath()
+            
+            context.fillPath()
+        }
+        
+        
     }
-    */
+
 
 }
