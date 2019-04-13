c48c089a07d690609075175bf801c6d18a8ae8a1
MB <mb@mbs-imac.local>
2019-04-13 19:59:47 +0530

    Removed comments done with drawing stuff now moving toward gestures

--- PlayingCard/PlayCardView.swift
+++ PlayingCard/PlayCardView.swift
@@ -8,33 +8,9 @@
 
 import UIKit
 
-//For viewing make custom View visible  in Interface Builder(IB)
-//This compiles your view and put in your IB
-//Custom View would appear blank because Images don't work with image name in Interface Builder
-//label are supported
-//well to support images in Interface Builder there exist another version of UIImage.init(named: "cardBack") which supports both on run and IB
-//UIImage.init(named: "cardBack",in : Bundle(for: self.classForCoder),compatibleWith : traitCollection)
-//support images for both IB and run
-//To make our class property available in interface builder we can use  @IBInspectable this way the variables are editable from Interface builder in attribute inspector
-// But we have to make the the variables that are @IBInspectable explicitly type
-// We cannot let this be inferred by swift .Because while swift is good at inference,Interface Builder isn't so much
-
 @IBDesignable
 class PlayCardView: UIView {
-    
-    
-    //but rank and suit where enum in model , but who care's this is a view it knows nothing about that model , this is a generic card drawing view.It knows nothing about that particular model
-    //So fact it represents rank and suit in completely different way perfectly fine
-    // whose job is it to translate model and view ofcourse controller
-    
-    //when you have var's in view that effect the view will draw you have to think about the fact if this changes the rank your view needs to redraw itself so  we will use didSet
-    
-    // setNeedsDisplay will cause drawRect called eventually
-    // so cant call drawRect directly we have to tell system we need to be displayed
-    
-    //Our view has anaother little thing that needs to happen. We have subviews to draw our part of view, so we need to have those subviews laid out.
-    //Now we are not using Auto Layout in our subviews we are putting them where they belong in the corners,but we still need to say "setNeedsLayout()"
-    // So our subviews can get laid out.Now you don't have to say this if you dont have any subviews that needs laying out or that aren't affected by the rank changing.In our case it definitely change the rank. So we are going to do that for all public vars here because if public change any of these things it's going to change the way our card looks
+
     @IBInspectable
     var rank : Int = 12 {didSet{setNeedsDisplay(); setNeedsLayout()}}
      @IBInspectable
@@ -44,19 +20,14 @@ class PlayCardView: UIView {
     
     private func centeredAttributedString(_ string : String,fontSize : CGFloat ) -> NSAttributedString{
         
-        //using preferredFont as its an user information not like system font or anything and body font and scaling to using .withSize(fontSize)
         var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
         
-        // for dynamically adjusting font size based on accessibility
         font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
         
-        // NSMutableParagraphStyle for aligning to center
-        // NSMutableParagraphStyle enscapsulate everything about paragraph like its alignment and things like that
         let paragraphStyle = NSMutableParagraphStyle()
-        // makes whole paragraph to be centered  paragraphStyle.alignment = .center
+
         paragraphStyle.alignment = .center
-        
-        //returning NSAttributedString with attributes
+
         return NSAttributedString(string: string, attributes: [.font : font , .paragraphStyle : paragraphStyle])
     }
     
@@ -64,21 +35,14 @@ class PlayCardView: UIView {
         return centeredAttributedString(rankString + "\n" + suit, fontSize: cornerFontSize)
     }
     
-    //Cannot use instance member 'createCornerLabel' within property initializer; property initializers run before 'self' is available
-    // Ofcourse we are initializing a variable over here "upperLeftCornerLabel " and calling a method on myself "createCornerLabel()"
-    // we know that until we are full initialized we cannot call mthods on ourself
-    // so we are going to use lazy (lazy makes it so these wont be initialized until they are asked for which will be after the thing is fully initialized)
     private lazy var upperLeftCornerLabel  : UILabel   = createCornerLabel()
     private lazy var lowerRightCornerLabel   = createCornerLabel()
     
     private func createCornerLabel() -> UILabel{
         let label = UILabel()
         
-        // byDefault label has 1 line ,
-        // by setting it to 0 it says use as many lines as you need
         label.numberOfLines = 0
-        
-        //adding label as a subview on customView
+
         addSubview(label)
         return label
     }
@@ -89,70 +53,29 @@ class PlayCardView: UIView {
         
         label.frame.size = CGSize.zero
         
-        //MARK:- SizeToFit Important thing to Know
-        //(****)       // it will size the label to fit its content by  label.sizeToFit()
-        //the only tricky thing about this is though is it if that label already has some width and you say sizeToFit, it will make it taller and keep the width.
-        // we dont want that , we wanted to do the whole thing
-        // so we do label.frame.size = CGSize.zero which clear out it's size before we do sizeToFit()
-        //this way it will expand in both directions , across and down
         label.sizeToFit()
-        
-        //hide card if not faceUp
-        //hidden keeps it in subviews list and everything,keeps it in right position just hides it
+
         label.isHidden = !isFaceUp
     }
-    
-    //called when our subview bounds change
+
     override func layoutSubviews() {
-        
-        //use super because UIView is awesome at laying out subviews.It uses autolayout.That all autolayout stuff UIView knows how to layout your subviews
+
         super.layoutSubviews()
-        
-        //Now this 2 subviews I am not doing any control dragging. I am creating them in code. So I have to do my layout myself and layoutSubviews is where you do it.
-        //Anytime your subviews needs to be laid out for any reasons this is going to get called by the system.You dont call it
-        //If you want it called you call setNeedsLayout()
-        //setNeedsLayout() -> the system will eventually call this layoutSubviews() just like
-        //setNeedsDisplay() -> the system will eventually call   func draw(_ rect: CGRect)
-        
+
         configureCornerLabel(upperLeftCornerLabel)
-        
-        //frame is used for position
-        //offsetBy(dx: cornerOffset, dy: cornerOffset) is our extension to move origin point by offset to avoid rounded corner
+
         upperLeftCornerLabel.frame.origin = bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
         
         
         
         configureCornerLabel(lowerRightCornerLabel)
-        
-        //MARK:- Transform i.e. Affine Transform
-        //transorm is what called an affine transform
-        //affine transfrom is really simple it's just a blob a thing that represents a scale, a translation and a rotation . Just those 3 things.
-        // So you can take a UIView and rotate,scale it and translate it all you want with just this one little var "transform"
-        //ofcourse we are positioning things with the frame and stuff like that , but this is an additional way to control it's positioning,scaling and rotating.Now this is all going to be bitwise translation. So if you make it bigger , it look kind of jaggy,edged , pixellated .
-        //But we aint gonna make it bigger instead we gonna make it rotate it.
-        
-        //btw transform only has 3 methods rotate,translate and scale
-        //CGAffineTransform.identity gives a identity matrix and we rotate it by pi radians
-        
-        //   lowerRightCornerLabel.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi)
-        //MARK:- 43.20 Multitouch https://www.youtube.com/watch?v=_ao1tlshRi0&list=PL3d_SFOiG7_8ofjyKzX6Nl1wZehbdiZC_&index=7
-        //has this wont work as it will rotate around origin so we have to translate it
-        
+
         lowerRightCornerLabel.transform = CGAffineTransform.identity.translatedBy(x: lowerRightCornerLabel.frame.size.width, y: lowerRightCornerLabel.frame.size.height).rotated(by: CGFloat.pi)
-        
-        //offsetting first to get origin inside rounded corner then offsetting to get inside by size of frame of label
+
         lowerRightCornerLabel.frame.origin = CGPoint(x: bounds.maxX, y: bounds.maxY).offsetBy(dx: -cornerOffset, dy: -cornerOffset).offsetBy(dx: -lowerRightCornerLabel.frame.size.width, dy: -lowerRightCornerLabel.frame.size.height)
         
         
     }
-    
-    //MARK:- TraitCollectionDidChange
-    // when we go on  General->accessibility->Large Text and change font of larger font size and come back to app it does have large font? That's weird .
-    //Well actually it does it's just it never redrew , if we change bounds and flip back  now we get to see large font So that's a problem.
-    //MARK:- "TraitCollectionDidChange" to track slider change
-    //When that slider moves we need to find out that it moved . And you can do that in view with a function called "TraitCollectionDidChange"
-    //traits have a lot to do with are you rotate,are you landscape are you portrait things like that are traits but also your size category is general for your font.
-    //So trait collection get called whenever those things change
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         setNeedsDisplay()
@@ -164,9 +87,7 @@ class PlayCardView: UIView {
         let pipsPerRowForRank = [[0], [1], [1,1], [1,1,1], [2,2], [2,1,2], [2,2,2], [2,1,2,2], [2,2,2,2], [2,2,1,2,2], [2,2,2,2,2]]
         
         func createPipString(thatFits pipRect: CGRect) -> NSAttributedString {
-            //getting max value of pipsPerRowForRank
             let maxVerticalPipCount = CGFloat(pipsPerRowForRank.reduce(0) { max($1.count, $0)})
-            //getting max value of pipsPerRowForRank element
             let maxHorizontalPipCount = CGFloat(pipsPerRowForRank.reduce(0) { max($1.max() ?? 0, $0)})
             let verticalPipRowSpacing = pipRect.size.height / maxVerticalPipCount
             let attemptedPipString = centeredAttributedString(suit, fontSize: verticalPipRowSpacing)
@@ -182,7 +103,6 @@ class PlayCardView: UIView {
         
         if pipsPerRowForRank.indices.contains(rank) {
             let pipsPerRow = pipsPerRowForRank[rank]
-            //cornerString size of cornerstring nsattribute String
             var pipRect = bounds.insetBy(dx: cornerOffset, dy: cornerOffset).insetBy(dx: cornerString.size().width, dy: cornerString.size().height / 2)
             let pipString = createPipString(thatFits: pipRect)
             let pipRowSpacing = pipRect.size.height / CGFloat(pipsPerRow.count)
@@ -199,14 +119,13 @@ class PlayCardView: UIView {
                 default:
                     break
                 }
-                //adding height space for next drawing
                 pipRect.origin.y += pipRowSpacing
             }
         }
     }
     
     override func draw(_ rect: CGRect) {
-        // bounds is bounds of custom View i.e. self i.e. PlayCardView
+
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
         
         roundedRect.addClip()
@@ -215,7 +134,7 @@ class PlayCardView: UIView {
 
         if isFaceUp{
             if let faceCardImage = UIImage.init(named: rankString+suit, in: Bundle(for: self.classForCoder), compatibleWith: traitCollection){
-                //.zoom is our extension method
+
                 faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
             }
             else{
@@ -224,8 +143,7 @@ class PlayCardView: UIView {
         }
         else{
             if let backgroundImage = UIImage.init(named: "cardBack",in : Bundle(for: self.classForCoder),compatibleWith : traitCollection){
-                
-                //drawing on whole bound as we dnt have any corner label
+
                 backgroundImage.draw(in: bounds)
             }
             
@@ -242,18 +160,14 @@ class PlayCardView: UIView {
 
 
 extension PlayCardView{
-    // we make a private struct we put it a name sometime we call it constants we called it SizeRatio because all of our constant are about ratio
-    //Constants are declared in private struct with static variables
+
     private struct SizeRatio{
         static let cornerFontSizeToBoundsHeight  : CGFloat = 0.085
         static let cornerRadiusToBoundsHeight    : CGFloat = 0.06
         static let cornerOffsetToCornerRadius    : CGFloat = 0.33
         static let faceCardImageSizeToBoundsSize : CGFloat = 0.75
     }
-    
-    //computed property
-    
-    // bounds.size.height gives the bounds height
+
     private var cornerRadius : CGFloat {return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight}
     
     private var cornerOffset : CGFloat {return cornerRadius * SizeRatio.cornerOffsetToCornerRadius}
@@ -296,8 +210,3 @@ extension CGPoint{
         return CGPoint(x: x+dx, y: y+dy)
     }
 }
-
-
-//safe area is the place it can draw without overriding or impinging upon other views space
-//bar button at bottom or title at top
-// we always create constraint from view's safe area
